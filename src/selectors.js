877b710f3e4cdac006bd8eae46131038e62ff18d
Miguel Angel <mgangsal@outlook.com>
2023-02-11 10:25:25 -0600

    Created selector file, and refactored background color (#4)

---
+++ src/selectors.js
@@ -0,0 +1,7 @@
+export const timeSvg = document.querySelector('#time-svg');
+export const clockDiv = document.querySelector('.clock');
+export const temperatureDiv = document.querySelector('.temp');
+export const addressDiv = document.querySelector('.address');
+export const countryDiv = document.querySelector('.country');
+export const cityDiv = document.querySelector('.city');
+export const changeLocationButton = document.querySelector('.change-location');
---
+++ src/utils/getBackgroundColor.js
@@ -0,0 +1,19 @@
+/** getBackgroundColor returns you a color based on the timeset */
+export default function getBrackgroundColor(userTime) {
+    if (userTime >= 5 && userTime <= 7) {
+        console.log('This is sunrise')
+        return '#FFDB00';
+    }
+    else if (userTime >= 8 && userTime <= 15) {
+        console.log('this is broad daylight')
+        return '#fff';
+    }
+    else if (userTime >= 16 && userTime <= 18) {
+        console.log('This is sunset')
+        return '#FE8714';
+    }
+    else if (userTime >= 19 && userTime <= 4) {
+        console.log('its nighttime')
+        return '#27445C';
+    }
+}
